ce18ce7a03f4887107ec9708f33d2d5af4dc524a
Shubham Mankar <themrshubh@gmail.com>
2020-07-04 20:24:23 +0530

    Added chartjs-demo project 📊

---
+++ chartjs-demo/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <b-container class="mt-3">
+      <b-row>
+        <b-col class="text-center">
+          <h3 class="text-muted">Demo ChartJS</h3>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-card style="width: 100%">
+          <chart></chart>
+        </b-card>
+      </b-row>
+      <b-row>
+        <b-card style="width: 100%" v-if="barSelected">
+          <h4 class="text-muted text-center">You selected {{month}}</h4>
+        </b-card>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import Chart from './components/Chart.vue'
+import { eventBus } from './main'
+
+export default {
+  name: 'App',
+  components: {
+    Chart
+  },
+  data() {
+    return {
+      barSelected: false,
+      month: 0
+    }
+  },
+  created() {
+    eventBus.$on('barClick', (month) => {
+      this.month = month
+      this.barSelected = true
+    })
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ chartjs-demo/src/components/Chart.vue
@@ -0,0 +1,61 @@
+<script>
+import { Bar } from 'vue-chartjs';
+import { eventBus } from '../main.js';
+
+export default {
+  mixins: [Bar],
+  data() {
+    return {
+      chartdata: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#1d3557',
+            data: [40, 20, 10, 50, 80, 10, 20, 70, 50, 91, 34, 100],
+            barPercentage: 0.9,
+            order: 3
+          },
+          {
+            label: 'Data One',
+            backgroundColor: '#a8dadc',
+            data: [50, 40, 30, 70, 50, 80, 50, 60, 30, 21, 34, 140],
+            barPercentage: 0.9,
+            order: 2
+          },
+          {
+            label: 'Line Dataset',
+            data: [10, 12, 15, 20, 30, 40, 55, 70, 95, 110, 120, 140 ],
+            backgroundColor: 'rgba(0, 0, 0, 0)',
+            borderColor: '#e63946',
+            type: 'line',
+            order: 1
+          },
+        ]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        onClick: this.clicked  
+        // scale: {
+        //     ticks: {
+        //         min : 0
+        //     }
+        // }
+      } 
+    }
+  },
+  mounted () {
+    this.renderChart(this.chartdata, this.options)
+  },
+  methods: {
+    clicked (event, arr) {
+      eventBus.$emit('barClick', this.chartdata.labels[arr[0]._index])
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
