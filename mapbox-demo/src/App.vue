99c6104f574357cd38dab05dcbbc85a34807c432
Shubham Mankar <themrshubh@gmail.com>
2020-07-08 11:20:09 +0530

    added mapbox demo 🤓

---
+++ mapbox-demo/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <b-container class="mt-4">
+      <b-row>
+        <b-col>
+          <h2 class="text-center">Mapbox Demo</h2>
+        </b-col>
+      </b-row>
+      <b-row class="mt-2">
+        <b-col>
+          <div>
+            <app-map></app-map>
+          </div>
+        </b-col>
+      </b-row>
+      <b-row class="mt-2">
+        <b-col>
+          <b-card style="width: 100%">
+            <div class="text-center">
+              More Map Info Here.
+            </div>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import Map from './components/Map.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'app-map': Map,
+  },
+}
+</script>
+
+<style>
+</style>
---
+++ mapbox-demo/src/components/Map.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <MglMap 
+    :accessToken="accessToken" 
+    :mapStyle="mapStyle"
+    :center="coordinates"
+    :zoom="zoom"
+    style="height: 350px"
+    >
+      <MglMarker style="cursor: pointer" :coordinates="coordinates" color="#c27474" @click="clicked">
+        <MglPopup :offset="offset">
+          <div>Hello, I'm popup!</div>
+        </MglPopup>
+      </MglMarker>
+    </MglMap>
+  </div>
+</template>
+
+<script>
+import Mapbox from "mapbox-gl";
+import { MglMap, MglMarker, MglPopup } from "vue-mapbox";
+
+export default {
+  components: {
+    MglMap,
+    MglMarker,
+    MglPopup
+  },
+  data(){
+    return {
+      accessToken: 'pk.eyJ1IjoidGhlbXJzaHViaCIsImEiOiJja2NibWNweW4xcTNrMnJwOGc4ODc4djcyIn0.aP1uv1MJm47-PkmcauOc5g',
+      mapStyle: 'mapbox://styles/themrshubh/ckcbp6qg76cjb1kputy5mkers/',
+      coordinates: [72.820059,19.044495],
+      zoom: 15,
+      offset:15
+    }
+  },
+  methods: {
+    clicked(){
+      console.log('Clicked!')
+    }
+  },
+  created() {
+    this.mapbox = Mapbox;
+  }
+}
+</script>
